b247a132a1b7bc659122f7a76620ddbb3a5c47a1
roceil <a0978006326@gmail.com>
2022-10-25 01:05:28 +0800

    header&footer 完成

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3F5D45',
+        secondary: '#FFE180',
+        light_primary: '#8DA291',
+        light_plus_primary: '#EAF0ED',
+      },
+      backgroundImage: {
+        'img-favorite01': "url('https://tina3295.github.io/sweet_taste/sweet%20homepage/images/SW02.png')",
+        'img-favorite02': "url('https://tina3295.github.io/sweet_taste/sweet%20homepage/images/SW05.png')",
+        'img-favorite03': "url('https://tina3295.github.io/sweet_taste/sweet%20homepage/images/SW03.png')",
+      }
+    },
+    container: {
+      center: true,
+      padding: '30px',
+    },
+    screens: {
+      'sm': '375px',
+      // => @media (min-width: 375px) { ... }
+  
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+  
+      'lg': '1000px',
+      // => @media (min-width: 1000px) { ... }
+    },
   },
+
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
